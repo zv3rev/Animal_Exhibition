02b45744c433d71c2b27d30657b606cc86b0177b
zv3rev <zv3rev624@gmail.com>
2023-04-23 16:52:39 +0300

    Initial commit

---
+++ src/main/java/com/vsu/app/AnimalExhibition.java
@@ -0,0 +1,11 @@
+package com.vsu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalExhibition {
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalExhibition.class,args);
+    }
+}
